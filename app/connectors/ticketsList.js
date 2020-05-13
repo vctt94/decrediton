a0443d16fe95f7b5ffed55ae084c28437a83a0c5
vctt94 <victor.duarte.oliveira@gmail.com>
2020-05-13 14:13:52 -0300

    [connectors] changes at multi connectors

--- app/connectors/ticketsList.js
+++ app/connectors/ticketsList.js
@@ -1,11 +1,12 @@
 import { connect } from "react-redux";
-import { selectorMap } from "../fp";
+import { selectorMap } from "fp";
 import { bindActionCreators } from "redux";
-import * as sel from "../selectors";
-import * as ca from "../actions/ClientActions";
+import * as sel from "selectors";
+import * as ca from "actions/ClientActions";
+import * as ta from "actions/TransactionActions";
 
 const mapStateToProps = selectorMap({
-  tickets: sel.tickets,
+  tickets: sel.stakeTransactions,
   tsDate: sel.tsDate,
   noMoreTickets: sel.noMoreTickets,
   ticketsFilter: sel.ticketsFilter,
@@ -14,8 +15,8 @@ const mapStateToProps = selectorMap({
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   goBackHistory: ca.goBackHistory,
-  getTickets: ca.getTickets,
-  changeTicketsFilter: ca.changeTicketsFilter
+  getTickets: ta.getTransactions,
+  changeTicketsFilter: ta.changeTicketsFilter
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps);
