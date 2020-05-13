e45480678439dbf92e94e7cd076f6d982875c4cc
vctt94 <victor.duarte.oliveira@gmail.com>
2020-05-13 14:14:13 -0300

    [constants] changes at constants

--- app/constants/Decred.js
+++ app/constants/Decred.js
@@ -19,7 +19,14 @@ export const TestNetParams = {
   HDCoinType: 1,
 
   TreasuryAddress: "TcrypGAcGCRVXrES7hWqVZb5oLJKCZEtoL1",
-  trezorCoinName: "Decred Testnet"
+  trezorCoinName: "Decred Testnet",
+
+  // Address encoding magics
+  NetworkAddressPrefix: "T",
+  PubKeyAddrID:         Buffer.from([ 0x28, 0xf7 ]), // starts with Tk
+  PubKeyHashAddrID:     Buffer.from([ 0x0f, 0x21 ]), // starts with Ts
+  PKHEdwardsAddrID:     [ 0x0f, 0x01 ], // starts with Te
+  PKHSchnorrAddrID:     [ 0x0e, 0xe3 ] // starts with TS
 };
 
 export const MainNetParams = {
@@ -37,7 +44,14 @@ export const MainNetParams = {
   HDCoinType: 42,
 
   TreasuryAddress: "Dcur2mcGjmENx4DhNqDctW5wJCVyT3Qeqkx",
-  trezorCoinName: "Decred"
+  trezorCoinName: "Decred",
+
+  // Address encoding magics
+  NetworkAddressPrefix: "D",
+  PubKeyAddrID:         [ 0x13, 0x86 ], // starts with Dk
+  PubKeyHashAddrID:     [ 0x07, 0x3f ], // starts with Ds
+  PKHEdwardsAddrID:     [ 0x07, 0x1f ], // starts with De
+  PKHSchnorrAddrID:     [ 0x07, 0x01 ] // starts with DS
 };
 
 // MAX_DCR_AMOUNT represents the maximum decred amount in atoms.
@@ -45,3 +59,19 @@ export const MAX_DCR_AMOUNT = 21e14;
 
 // UNIT_DIVISOR represents the minimum value one decred can have in atoms.
 export const UNIT_DIVISOR = 100000000;
+
+
+// STEcdsaSecp256k1 specifies that the signature is an ECDSA signature
+// over the secp256k1 elliptic curve.
+export const	STEcdsaSecp256k1 = 0;
+
+// STEd25519 specifies that the signature is an ECDSA signature over the
+// edwards25519 twisted Edwards curve.
+export const	STEd25519 = 1;
+
+// STSchnorrSecp256k1 specifies that the signature is a Schnorr
+// signature over the secp256k1 elliptic curve.
+export const STSchnorrSecp256k1 = 2;
+
+// ripemd160Size is the size of the RIPEMD-160 hash algorithm checksum in bytes.
+export const ripemd160Size = 20;
